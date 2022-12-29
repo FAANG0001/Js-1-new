5d62947209f641b914d7ba064d7070f4e2b15995
Mirabbos <m.tohirov@gmail.com>
2022-12-29 13:51:00 +0500

    Js-1 DZ

--- main.js
+++ main.js
@@ -1,40 +1,43 @@
 
 
-var str = prompt('Введите свое имя')
+var imya = prompt('Введите имя');
+var age = prompt('Введите возраст');
+var plus = +prompt('25 + 80 ');
+var minus = +prompt('2555 - 800 ');
+var mult = +prompt('10 * 15 ');
+var devide = +prompt('100 / 80 ');
+var percent = +prompt('1000 % 50 ');
+
 
 var imya = 'Abbos'
 
 console.log('Меня зовут ' + imya );
 
 
-
-var str = +prompt('Введите аш возвраст')
-
 var age = 29
-
 
 console.log('Мне ' + age );
 
 
 var str = +prompt(' Решите пример 25 + 80 = ?')
 
-console.log('Пример 1 ' + '25 + 80 = ' + (25 + 80 ) + ' Ваш ответ ');
+console.log('Пример 1 ' + '25 + 80 = ' + (25 + 80 ) + ' Ваш ответ ' + plus);
 
 var str = +prompt(' Решите пример 255 - 800 = ?')
 
-console.log('Пример 2 ' + '2555 - 800 = ' + (2555 - 800 ) + ' Ваш ответ ');
+console.log('Пример 2 ' + '2555 - 800 = ' + (2555 - 800 ) + ' Ваш ответ ' + minus);
 
 var str = +prompt(' Решите пример 10 * 15 = ?')
 
-console.log('Пример 3 ' + '10 * 15 = ' + (10 * 15 ) + ' Ваш ответ ');
+console.log('Пример 3 ' + '10 * 15 = ' + (10 * 15 ) + ' Ваш ответ ' + mult);
 
 var str = +prompt(' Решите пример 100 / 80 = ?')
 
-console.log('Пример 4 ' + '100 / 80 = ' + (100 / 80 ) + ' Ваш ответ ');
+console.log('Пример 4 ' + '100 / 80 = ' + (100 / 80 ) + ' Ваш ответ ' + devide);
 
 var str = +prompt(' Решите пример 1000 % 50 = ?')
 
-console.log('Пример 5 ' + '1000 % 50 = ' + (1000 % 50 ) + ' Ваш ответ ');
+console.log('Пример 5 ' + '1000 % 50 = ' + (1000 % 50 ) + ' Ваш ответ ' + percent);
 
 
 
